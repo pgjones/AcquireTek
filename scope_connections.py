81e8bf1936ec63330afa799cf9ec7b8b8f447eff
pgjones <p.g.jones@qmul.ac.uk>
2013-06-01 14:40:53 +0100

    Added untested tcp/ip scope connection.

--- scope_connections.py
+++ scope_connections.py
@@ -9,6 +9,7 @@
 from pyvisa.vpp43 import visa_library, visa_exceptions
 visa_library.load_library("/Library/Frameworks/Visa.framework/VISA") # Mac specific??
 import visa
+import socket
 
 class VisaUSB(object):
     """ Connect via visa/usb."""
@@ -29,3 +30,34 @@ class VisaUSB(object):
         except visa_exceptions.VisaIOError:
             # No answer given
             return None
+
+class TCPIP(object):
+    """ Connect via TCP/IP i.e. ethernet."""
+    def __init__(self, ip, port):
+        """ Connect with the ip and port address."""
+        self._connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._connection.connect((ip, port))
+        self._connection.setblocking(False)
+        print "Connecting to %s:%i" % (ip, port)
+        print "Which had identity:", self.ask("*idn?")
+    def __del__(self):
+        """ Close the connection."""
+        self._connection.close()
+    def send(self, command):
+        """ Send a command, doesn't expect a returned result."""
+        self._connection.send(command)
+
+    def ask(self, command):
+        """ Send a command and expect an answer."""
+        self._connection.send(command)
+        response = ''
+        while True:
+            char = ""
+            try:
+                char = self._connection.recv(1)
+            except:
+                if response.rstrip() != "":
+                    break
+            if char:
+                response += char
+        return response.rstrip()
