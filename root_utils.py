04c492c1cf79b856a508d74055112602bc148aca
pgjones <p.g.jones@qmul.ac.uk>
2013-06-07 15:28:37 +0100

    More specific utilities.

--- root_utils.py
+++ root_utils.py
@@ -7,8 +7,6 @@
 # Author P G Jones - 04/06/2013 <p.g.jones@qmul.ac.uk> : First revision
 #################################################################################################### 
 import ROOT
-import array
-c1 = ROOT.TCanvas()
 
 def waveform_to_hist(data_forms, data_units, title="hist"):
     """ Pass a tuple of dataforms and data units."""
@@ -18,30 +16,7 @@ def waveform_to_hist(data_forms, data_units, title="hist"):
         histogram.SetBinContent(index + 1, data)
     histogram.GetXaxis().SetTitle(data_units[0])
     histogram.GetYaxis().SetTitle(data_units[1])
-    histogram.Draw()
-    c1.Update()
     return histogram
-
-def integrate_signal(histogram, trigger=0.0):
-    """ Return the integral of the signal pulse, assumed to be at trigger."""
-    zero_bin = histogram.GetXaxis().FindBin(0.0) # Find the bin for time 0 (trigger)
-    center_bin = zero_bin
-    for bin in range(zero_bin, histogram.GetNbinsX()):
-        if histogram.GetBinContent(bin) > trigger:
-            centre_bin = bin
-            break
-        if bin == histogram.GetNbinsX() - 1:
-            return 0.0 # No Trigger
-    sig = [centre_bin, centre_bin + 1] # Signal start/end
-    for bin in range(centre_bin, 0, -1): # Find signal start
-        if histogram.GetBinContent(bin) <= 0.0:
-            sig[0] = bin
-            break
-    for bin in range(centre_bin, histogram.GetNbinsX()):
-        if histogram.GetBinContent(bin) <= 0.0:
-            sig[1] = bin
-            break
-    return histogram.Integral(sig[0], sig[1])
 
 class WaveformFile(object):
     """ A root file filled with waveforms."""
@@ -61,27 +36,6 @@ class WaveformFile(object):
         """ Save the data to the ttree."""
         waveform_to_hist(data_forms, data_units)
         self._hists[channel] = waveform_to_hists(data_forms, data_units)
-    def save(self):
-        """ Save the current hists."""
-        self._tree.Fill()
-
-class ValueFile(object):
-    """ A root file filled with integral values."""
-    def __init__(self, file_path, channels):
-        """ Initialise by opening the root file and specifying the number of channels to save."""
-        self._file = ROOT.TFile(file_path, "RECREATE")
-        self._tree = ROOT.TTree("T", "Data tree")
-        self._values = {}
-        for channel in range(1, channels + 1):
-            self._values[channel] = array.array('f', [0])
-            self._tree.Branch("channel_%i" % channel, self._values[channel], "value/F")
-    def close(self):
-        """ Close the file."""
-        self._tree.Write()
-        self._file.Close()
-    def set_data(self, data, channel):
-        """ Save the data to the ttree."""
-        self._values[channel] = data
     def save(self):
         """ Save the current hists."""
         self._tree.Fill()
--- utils.py
+++ utils.py
@@ -2,32 +2,11 @@
 #
 # utils.py
 #
-# Utility functions to integrate and save waveforms in pickle format.
+# Utility functions to save waveforms in pickle format.
 #
 # Author P G Jones - 06/06/2013 <p.g.jones@qmul.ac.uk> : First revision
 #################################################################################################### 
-import numpy
 import pickle
-
-def integrate_signal(data_forms, trigger=0.0):
-    """ Return the integrals of any pulses above the trigger."""
-    integrals = []
-    for bin in range(0, len(data_forms[1])):
-        if data_forms[1][bin] > trigger:
-            integral = 0.0
-            # First go back and find early integral
-            for pre_bin in range(bin, -1):
-                if data_forms[1][pre_bin] <= trigger / 2.0:
-                    break
-                integral += data_forms[1][pre_bin]
-            # Now go forwards and find full integral
-            for post_bin in range(bin, len(data_forms[1])):
-                if data_forms[1][post_bin] <= trigger / 2.0:
-                    bin = post_bin # Start from here for next search
-                    break
-                integral += data_forms[1][post_bin]
-            integrals.append(integral)
-    return integrals
 
 class File(object):
     """ A pickle file."""
