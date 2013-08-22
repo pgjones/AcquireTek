4925e462c0356c84cb55cbfbeb62bbed0efabd90
pgjones <p.g.jones@qmul.ac.uk>
2013-08-22 13:21:56 +0100

    Extra meta data

--- acquire_pmt_data.py
+++ acquire_pmt_data.py
@@ -21,20 +21,23 @@ tek_scope.set_invert_channel(1, True) # Invert both channels
 tek_scope.set_invert_channel(2, True) # Invert both channels
 trigger = -0.020 # Volts
 trigger_channel = 1
+ymult = 20e-3 # Volts
 tek_scope.set_edge_trigger(trigger, trigger_channel, True) # Falling edge trigger
 tek_scope.set_channel_coupling(1, "ac")
 tek_scope.set_channel_coupling(2, "ac")
-tek_scope.set_channel_y(1, 20e-3)
-tek_scope.set_channel_y(2, 20e-3)
+tek_scope.set_channel_y(1, ymult)
+tek_scope.set_channel_y(2, ymult)
 tek_scope.set_data_mode(49500, 50500)
 tek_scope.lock() # Re acquires the preamble
 # Now create a root file to save 2 channel data in
 results = utils.HDF5File("results", 2)
 results.set_meta_data("trigger", trigger)
 results.set_meta_data("trigger_channel", trigger_channel)
+results.set_meta_data("channel_y_mult", ymult)
 results.set_meta_data("timeform_1", tek_scope.get_timeform(1))
 results.set_meta_data("timeform_2", tek_scope.get_timeform(2))
-#results.set_meta_dict(tek_scope.get_preamble())
+results.set_meta_data("ch1", tek_scope._get_preamble())
+results.set_meta_data("ch2", tek_scope._get_preamble())
 
 t_start = time.time()
 acquire_time = int(sys.argv[1]) * 60 # in seconds
