665ec3626085a16cfba9d335b2de6b6b62d84de6
bmurrells <bmurrells@hotmail.co.uk>
2013-08-22 17:46:32 +0100

    Various bug fixes.

--- acquire_pmt_data.py
+++ acquire_pmt_data.py
@@ -17,9 +17,12 @@ import datetime
 from pyvisa.vpp43 import visa_exceptions
 
 duration=int(sys.argv[1])
-interval=float(sys.argv[2])*60
+if duration > 60:
+    interval=60 * 60
+else:
+    interval=duration * 60
 milestone=interval
-kind=sys.argv[4]
+kind=sys.argv[3]
 today=str(datetime.date.today())
 
 usb_conn = scope_connections.VisaUSB()
@@ -34,18 +37,19 @@ name='%s_%d_hour_%s' %(today,duration/60,kind)
 roll=os.walk('results').next()[1]
 for i in roll:
     if i==name:
-        name='%s_%d_hour_%s%d' %(today,duration/60,kind,count)
+        name='%s_%d_hour_%s_%d' %(today,duration/60,kind,count)
         for i in roll:
             if i==name:
                 count+=1
                 i=roll[0]
 os.chdir('results')
+print "Saving to", name
 os.mkdir(name)
 os.chdir(name)
 
-trigger = -0.1 # Volts
-trigger_channel = 1
-ymult = 20e-3 # Volts
+trigger = -0.004 # Volts
+trigger_channel = 2
+ymult = 100e-3 # Volts
 tek_scope.set_edge_trigger(trigger, trigger_channel, True) # Falling edge trigger
 tek_scope.set_channel_coupling(1, "ac")
 tek_scope.set_channel_coupling(2, "ac")
@@ -60,14 +64,15 @@ results.set_meta_data("trigger_channel", trigger_channel)
 results.set_meta_data("channel_y_mult", ymult)
 results.set_meta_data("timeform_1", tek_scope.get_timeform(1))
 results.set_meta_data("timeform_2", tek_scope.get_timeform(2))
-results.set_meta_data("ch1", tek_scope._get_preamble())
-results.set_meta_data("ch2", tek_scope._get_preamble())
+results.set_meta_data("ch1", tek_scope._get_preamble(1))
+results.set_meta_data("ch2", tek_scope._get_preamble(2))
 
-time.sleep(float(sys.argv[3])*60)
+print "Waiting", sys.argv[2], "seconds to record", duration, " minutes in", interval, "second files"
+time.sleep(float(sys.argv[2])*60)
 
 t_start = time.time()
 acquire_time = duration * 60 # in seconds
-print "Started at", t_start
+print "Started at", datetime.datetime.now().time()
 
 filecount=0
 while time.time()-t_start<acquire_time:
@@ -90,7 +95,7 @@ while time.time()-t_start<acquire_time:
             print "Serious death"
             time.wait(1)
         print "Time left:", acquire_time - (time.time() - t_start), "s"
-    
+    print "New file at", time.time()-t_start
     milestone+=interval
 
     results.save()
